5e81a9a5ad64228496a2291c6659546da2c1fdf1
power <power@powerdeMacBook-Pro.local>
2023-03-26 13:26:47 +0800

    更改打包方式

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import { viteMockServe } from 'vite-plugin-mock'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist',
+    assetsDir: 'dist/assets'
+  },
   plugins: [vue(),vueJsx(),viteMockServe({
     // default
     mockPath: 'mock',
